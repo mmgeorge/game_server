c34310cf97c82468fc289f6be45ab30c1c0c4970
jeanettepranin <jeanettepranin@gmail.com>
2017-05-30 23:25:14 -0500

    add rust docs

---
+++ target/doc/implementors/url/percent_encoding/trait.EncodeSet.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["game_server"] = ["impl <a class=\"trait\" href=\"https://docs.rs/url/1.4.0/url/percent_encoding/trait.EncodeSet.html\" title=\"trait url::percent_encoding::EncodeSet\">EncodeSet</a> for <a class=\"struct\" href=\"https://hyper.rs/hyper/v0.10.11/hyper/header/parsing/struct.HTTP_VALUE.html\" title=\"struct hyper::header::parsing::HTTP_VALUE\">HTTP_VALUE</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ target/doc/search-index.js
@@ -0,0 +1,3 @@
+var searchIndex = {};
+searchIndex["game_server"] = {"doc":"Creates a game server that listens on port 8080; GET  localhost:8080/api/connect_four.svc/$metadata ->  metadata GET  localhost:8080/api/connect_four.svc/Games     ->  list of games GET  localhost:8080/api/connect_four.svc/Games(0)  ->  game with id=0 POST localhost:8080/api/connect_four.svc/Games     ->  create a new game      with body = {\"curr_player\": 1, \"height\": 7, \"id\": 4, \"k\": 4,\"width\": 5 }","items":[],"paths":[]};
+initSearch(searchIndex);
